4a4732a12c06b79252c403edde95107e315a34fe
argotnaut <161991025+argotnaut@users.noreply.github.com>
2026-06-27 22:00:52 -0400

    Added semicircle Truchet tiling

--- tiling/tiling.js
+++ tiling/tiling.js
@@ -1,7 +1,12 @@
 // Draw a random Truchet tiling on the canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-let tileSize = 50; // you can adjust this
+// Colors used for the two halves of each tile
+let color1 = "#5cb6ff";
+let color2 = "#71d9ff";
+
+
+let tileSize = 50; // default tile size
 
 function resizeCanvas() {
   canvas.width = window.innerWidth;
@@ -14,54 +19,123 @@ function circularShiftArray(inputArray, nShifts) {
   return inputArray.slice(splitIdx).concat(inputArray.slice(0, splitIdx));
 }
 
+function drawTriangles(i, j) {
+  const x = j * tileSize;
+  const y = i * tileSize;
+
+  /* 
+      The points around the tile. The drawing code will walk through this
+      array in order as it's drawing the two triangles
+      */
+  cornersMatrix = [
+    [0, 0], // top-left
+    [tileSize, 0], // top-right
+    [tileSize, tileSize], // bottom-right
+    [0, tileSize], // bottom left
+  ];
+  /*
+      Shift the cornersMatrix that the drawing code will walk through so it
+      starts at the random index
+      */
+  const shiftedCornersMatrix = circularShiftArray(
+    cornersMatrix,
+    Math.round(Math.random() * 3),
+  );
+
+  // Draw first triangle with color1
+  ctx.fillStyle = color1;
+  ctx.beginPath();
+  ctx.moveTo(x + shiftedCornersMatrix[0][0], y + shiftedCornersMatrix[0][1]);
+  ctx.lineTo(x + shiftedCornersMatrix[1][0], y + shiftedCornersMatrix[1][1]);
+  ctx.lineTo(x + shiftedCornersMatrix[2][0], y + shiftedCornersMatrix[2][1]);
+  ctx.closePath();
+  ctx.fill();
+
+  // (The second triangle will be the negative space in the background)
+}
+
+function drawQuarterCircles(i, j) {
+  const x = j * tileSize;
+  const y = i * tileSize;
+
+  const topLeftBottomRight = {
+    corners: [
+      {x: 0, y: 0},
+      {x: tileSize, y: tileSize}
+    ],
+    angles: [
+      { start: 0, end: 1/2 * Math.PI},
+      { start: Math.PI, end: 3/2 * Math.PI}
+    ]
+  };
+  const topRightBottomLeft = {
+    corners: [
+      {x: tileSize, y: 0},
+      {x: 0, y: tileSize}
+    ],
+    angles: [
+      {start: 1/2 * Math.PI, end: Math.PI},
+      {start: 3/2 * Math.PI, end: 0}
+    ]
+  };
+  
+  orientation = topLeftBottomRight;
+  if (Math.random() > 0.5) {
+    orientation = topRightBottomLeft;
+  }
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = color1;
+  ctx.beginPath();
+  ctx.arc(
+    x + orientation.corners[0].x,
+    y + orientation.corners[0].y,
+    0.5 * tileSize,
+    orientation.angles[0].start,
+    orientation.angles[0].end
+  );
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(
+    x + orientation.corners[1].x,
+    y + orientation.corners[1].y,
+    0.5 * tileSize,
+    orientation.angles[1].start,
+    orientation.angles[1].end,
+  );
+  
+  ctx.stroke();
+}
+
 function drawTiling() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.style.background = color2;
   const rows = Math.ceil(canvas.height / tileSize);
   const cols = Math.ceil(canvas.width / tileSize);
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      const x = j * tileSize;
-      const y = i * tileSize;
-
-      /* 
-      The points around the tile. The drawing code will walk through this
-      array in order as it's drawing the two triangles
-      */
-      pointsMatrix = [
-        [0, 0], // top-left
-        [tileSize, 0], // top-right
-        [tileSize, tileSize], // bottom-right
-        [0, tileSize], // bottom left
-      ];
-      /*
-      Shift the pointsMatrix that the drawing code will walk through so it
-      starts at the random index
-      */
-      const shiftedPointsMatrix = circularShiftArray(
-        pointsMatrix,
-        Math.round(Math.random() * 3),
-      );
-
-      // Randomly choose the color
-      const color = Math.random() < 0.5 ? -1 : 1;
-      const getColor = (num) => (num > 0 ? "#000000" : "#FF0000");
-      // Draw the tile
-      ctx.fillStyle = getColor(color); // use the randomly chosen color
-      ctx.beginPath();
-      ctx.moveTo(x + shiftedPointsMatrix[0][0], y + shiftedPointsMatrix[0][1]);
-      ctx.lineTo(x + shiftedPointsMatrix[1][0], y + shiftedPointsMatrix[1][1]);
-      ctx.lineTo(x + shiftedPointsMatrix[2][0], y + shiftedPointsMatrix[2][1]);
-      ctx.closePath();
-      ctx.fill(); // Halfway through the array of points, close the first triangle and start the next one
-      ctx.fillStyle = getColor(color * -1); // use the other color
-      ctx.beginPath();
-      ctx.moveTo(x + shiftedPointsMatrix[2][0], y + shiftedPointsMatrix[2][1]);
-      ctx.lineTo(x + shiftedPointsMatrix[3][0], y + shiftedPointsMatrix[3][1]);
-      ctx.lineTo(x + shiftedPointsMatrix[0][0], y + shiftedPointsMatrix[0][1]);
-      ctx.closePath();
-      ctx.fill(); // At the end of the array of points, close the second triangle at the start of the first one
+      // drawTriangles(i, j);
+      drawQuarterCircles(i, j);
     }
   }
 }
+
+document.getElementById("color1").setAttribute("value", color1)
+document.getElementById("color1").addEventListener("input", (e) => {
+  color1 = e.target.value;
+  drawTiling();
+});
+document.getElementById("color2").setAttribute("value", color2)
+document.getElementById("color2").addEventListener("input", (e) => {
+  color2 = e.target.value;
+  drawTiling();
+});
+document.getElementById("tileSize").addEventListener("input", (e) => {
+  const val = parseInt(e.target.value, 10);
+  if (val > 0) {
+    tileSize = val;
+    resizeCanvas();
+  }
+});
 
 window.addEventListener("resize", resizeCanvas);
 window.addEventListener("load", resizeCanvas);
